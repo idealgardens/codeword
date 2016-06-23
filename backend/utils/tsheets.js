6fd11c5c28f9057184c06fb04b66381af85b5404
prescottprue <sprue.dev@gmail.com>
2016-06-23 00:47:44 -0700

    Fixes deployment error. Removes unused deps.
    * Fixes deployment error by fixing webpack config and removing
    reduxReactFirebase from prod store creation.
    * Removes unused deps

--- backend/utils/tsheets.js
+++ backend/utils/tsheets.js
@@ -1,7 +1,6 @@
 import TSheets from 'tsheets-client'
 
 export const getSheets = () => {
-  console.log('tsheets:', TSheets)
   const api_token = process.env.TSHEETS_TOKEN
   const start_date = '2015-01-01'
   const end_date = '2016-01-01'
