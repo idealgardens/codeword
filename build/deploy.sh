1446394834c652a268774ab3ae53cd307d72716a
prescottprue <sprue.dev@gmail.com>
2016-09-05 16:38:47 -0700

    replaced Firebase deploy through shell with node

--- build/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-npm get prefix
-firebase --version
-
-if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  firebase deploy --token $FIREBASE_TOKEN
-fi
